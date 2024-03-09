3f287dd39296eeaf34a2bf30d93a7f7752d8281d
Nathan Rodet <rodetnathan.contact@gmail.com>
2024-03-09 13:39:12 +0100

    feat: made creating ticket for admin only

--- contracts/Ticket.sol
+++ contracts/Ticket.sol
@@ -18,19 +18,21 @@ contract Ticket is ERC721Enumerable {
     mapping(uint256 => TicketInfo) public tickets;
     mapping(address => bool) public admins;
 
-    event AdminAdded(address indexed newAdmin);
-    event AdminRemoved(address indexed removedAdmin);
     event TicketCreated(uint256 indexed ticketId, string name);
     event TicketBought(uint256 indexed ticketId, address indexed buyer);
 
+    modifier onlyAdmin() {
+        require(admins[msg.sender], "Caller is not an admin");
+        _;
+    }
+
     constructor() ERC721("Ticket", "TCKTPRC") {
-        
         ticketPrice[TicketPrice.Bus] = 1;
         ticketPrice[TicketPrice.Subway] = 2;
         ticketPrice[TicketPrice.Train] = 3;
     }
 
-    function createTicket(string memory name, TicketPrice ticketPrice, string memory imageUrl, string memory description) public {
+    function createTicket(string memory name, TicketPrice ticketPrice, string memory imageUrl, string memory description) public onlyAdmin {
         tickets[_nextTicketId] = TicketInfo(name, ticketPrice, imageUrl, description);
         emit TicketCreated(_nextTicketId, name);
         _nextTicketId++;
