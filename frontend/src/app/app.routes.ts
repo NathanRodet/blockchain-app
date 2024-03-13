c34ab42a2217ed032365a212749055ec43ea9bb2
obenchekro <othmanebenchekroun@outlook.fr>
2024-03-13 21:24:07 +0100

    (tiny)feat: routing established + binded a 404 page error

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { PrivilegeCardListComponent } from './list-privilege-card/list-privilege-card.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'privilege-cards', component: PrivilegeCardListComponent }
+  {
+    path: 'privilege-cards',
+    children: [
+      { path: 'purchase', component: PrivilegeCardListComponent },
+      { path: '', component: NotFoundComponent },
+      { path: '**', component: NotFoundComponent }
+    ]
+  },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
